a547aa8ed970f1b396554f13d48e79fde7eb7268
Pascal Otto <uk058821@student.uni-kassel.de>
2020-07-15 20:58:51 +0200

    test motor control pins

--- iesrobot.c
+++ iesrobot.c
@@ -6,11 +6,44 @@
 #include <util/delay.h>
 #include <stdio.h>
 
+#define UNK1_DDR    DDRD
+#define UNK1_PORT   PORTD
+#define UNK1_PIN    PD5
+
+#define UNK2_DDR    DDRD
+#define UNK2_PORT   PORTD
+#define UNK2_PIN    PD7
+
+#define UNK3_DDR    DDRB
+#define UNK3_PORT   PORTB
+#define UNK3_PIN    PB0
+
+#define UNK4_DDR    DDRB
+#define UNK4_PORT   PORTB
+#define UNK4_PIN    PB1
+
+#define UNK5_DDR    DDRB
+#define UNK5_PORT   PORTB
+#define UNK5_PIN    PB3
+
+#define UNK6_DDR    DDRD
+#define UNK6_PORT   PORTD
+#define UNK6_PIN    PD6
+
 int main(void)
 {
     led_init();
     USART_Init(UBRR_SETTING);
     linetracker_init();
+    
+    UNK1_DDR |= (1 << UNK1_PIN);
+    UNK2_DDR |= (1 << UNK2_PIN);
+    UNK3_DDR |= (1 << UNK3_PIN);
+    UNK4_DDR |= (1 << UNK4_PIN);
+    UNK5_DDR |= (1 << UNK5_PIN);
+    UNK6_DDR |= (1 << UNK6_PIN);
+    
+    
     
     /*while(1) {
         char readout [100];
@@ -34,7 +67,7 @@ int main(void)
         
         _delay_ms(1000);
     }*/
-    while(1) {
+    /*while(1) {
         bool left_triggered = linetracker_is_triggered(LEFTTRACKER);
         bool middle_triggered = linetracker_is_triggered(MIDTRACKER);
         bool right_triggered = linetracker_is_triggered(RIGHTTRACKER);
@@ -55,6 +88,34 @@ int main(void)
         }
         else {
 
+        }
+    }*/
+    while(1) {
+        unsigned char recv = USART_ReceiveByte();
+        switch(recv) {
+            case '1':
+                UNK1_PORT ^= (1 << UNK1_PIN);
+                break;
+            case '2':
+                UNK2_PORT ^= (1 << UNK2_PIN);
+                break;
+            case '3':
+                UNK3_PORT ^= (1 << UNK3_PIN);
+                break;
+            case '4':
+                UNK4_PORT ^= (1 << UNK4_PIN);
+                break;
+            case '5':
+                UNK5_PORT ^= (1 << UNK5_PIN);
+                break;
+            case '6':
+                UNK6_PORT ^= (1 << UNK6_PIN);
+                break;
+            case '\n': 
+                break;
+            default:
+                USART_print("wrong input\n");
+                break;
         }
     }
 }
