d86777d3e3149cc3234df36e80bf5c89a5a5510b
Pascal Otto <uk058821@student.uni-kassel.de>
2020-07-16 18:58:21 +0200

    improve movement

--- iesrobot.c
+++ iesrobot.c
@@ -16,57 +16,47 @@ int main(void)
     
     motorcontrol_enable(true);
     
-    uint32_t no_track_timeout = 2500;
-    /*while(1) {
-        char readout [100];
-        float voltage = linetracker_measure_wait(LEFTTRACKER) / 1024.f * 5.f;
-        dtostrf(voltage, 3, 3, readout);
-        USART_print("Measure1: ");
-        USART_print(readout);
-        USART_print("\n");
-        
-        voltage = linetracker_measure_wait(MIDTRACKER) / 1024.f * 5.f;
-        dtostrf(voltage, 3, 3, readout);
-        USART_print("Measure2: ");
-        USART_print(readout);
-        USART_print("\n");
-        
-        voltage = linetracker_measure_wait(RIGHTTRACKER) / 1024.f * 5.f;
-        dtostrf(voltage, 3, 3, readout);
-        USART_print("Measure3: ");
-        USART_print(readout);
-        USART_print("\n\n\n\n");
-        
-        _delay_ms(1000);
-    }*/
+    uint32_t no_track_timeout = 5000;
+    motorcontrol_direction prev_left = STOP;
+    motorcontrol_direction prev_right = STOP;
     while(1) {
         bool left_triggered = linetracker_is_triggered(LEFTTRACKER);
         bool middle_triggered = linetracker_is_triggered(MIDTRACKER);
         bool right_triggered = linetracker_is_triggered(RIGHTTRACKER);
         if(left_triggered || middle_triggered || right_triggered) {
-            no_track_timeout = 2500;
+            no_track_timeout = 5000;
         }
         if(left_triggered && !right_triggered) { // steer left
             led_set_blue(true);
             led_set_green(false);
             led_set_yellow(false);
-            motorcontrol_drive(BACKWARD, FORWARD);
+            prev_left = BACKWARD;
+            prev_right = FORWARD;
+            motorcontrol_drive(prev_left, prev_right);
         }
         else if(!left_triggered && right_triggered) { // steer right
             led_set_blue(false);
             led_set_green(false);
             led_set_yellow(true);
-            motorcontrol_drive(FORWARD, BACKWARD);
+            prev_left = FORWARD;
+            prev_right = BACKWARD;
+            motorcontrol_drive(prev_left, prev_right);
 
         }
-        else if(middle_triggered) { // forward
+        else if(middle_triggered && !left_triggered && !right_triggered) { // forward
             led_set_blue(false);
             led_set_green(true);
             led_set_yellow(false);
             motorcontrol_drive(FORWARD, FORWARD);
         }
-        else if(!left_triggered && !middle_triggered && !right_triggered) {
+        else if(!left_triggered && !middle_triggered && !right_triggered) { // no track detected
             no_track_timeout--;
+            if(no_track_timeout >= 3500) {
+                
+            }
+            else if(no_track_timeout <= 3500) {
+                motorcontrol_drive(prev_left, prev_right);
+            }
             if(no_track_timeout <= 0) {
                 motorcontrol_enable(false);
                 led_set_green(true);
